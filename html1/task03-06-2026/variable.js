4ea2b31177bf31cd3924c9ef8ba6934e094daab4
Abhiram1444 <abhiramreddy144@gmail.com>
2026-06-03 17:56:39 +0530

    html and js

---
+++ html1/task03-06-2026/variable.js
@@ -0,0 +1,26 @@
+let name = "Abhiram";
+
+console.log(name);
+let age = 22;
+
+console.log("Age:", age);
+let num1 = 10;
+let num2 = 20;
+
+let sum = num1 + num2;
+
+console.log("Sum =", sum);
+let principal = 1000;
+let rate = 5;
+let time = 2;
+
+let simpleInterest = (principal * rate * time) / 100;
+
+console.log("Simple Interest =", simpleInterest);
+let studentName = "Rahul";
+let course = "JavaScript";
+let marks = 90;
+
+console.log("Name:", studentName);
+console.log("Course:", course);
+console.log("Marks:", marks);
