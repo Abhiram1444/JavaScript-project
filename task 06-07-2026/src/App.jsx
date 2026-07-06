022ea5f2d58f6df9d9de916dff025ac28548d4f1
Abhiram1444 <abhiramreddy144@gmail.com>
2026-07-06 17:57:04 +0530

    react

---
+++ task 06-07-2026/src/App.jsx
@@ -0,0 +1,35 @@
+//import Home from "./Home"
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Abhiram React</h1>
+      <p>This is my first React application.</p>
+      <div>
+      <h1>Personal Information</h1>
+
+      <h2>Name: Abhiram</h2>
+
+      <p>Age: 24</p>
+
+      <p>City: Hyderabad</p>
+
+      <p>Course: React Development</p>
+
+      <p>Company: NYB INFOTECH</p>
+
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+
+      <button>Contact Me</button>
+    </div>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ task 06-07-2026/src/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>My first React component</p>
+    </div>
+  )
+}
+
+export default Home
