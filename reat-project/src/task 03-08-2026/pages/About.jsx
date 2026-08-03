6ecb6e66dde23d7b59544ee92fb3085634710b20
Abhiram1444 <abhiramreddy144@gmail.com>
2026-08-03 17:54:07 +0530

    browser routing

---
+++ reat-project/src/task 03-08-2026/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We provide React Training.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ reat-project/src/task 03-08-2026/pages/Services.jsx
@@ -0,0 +1,18 @@
+function Services() {
+  return (
+    <div>
+      <h1>Our Services</h1>
+
+      <h3>React Development</h3>
+      <p>We build modern React applications.</p>
+
+      <h3>Web Development</h3>
+      <p>We develop responsive websites.</p>
+
+      <h3>UI/UX Design</h3>
+      <p>We design user-friendly interfaces.</p>
+    </div>
+  );
+}
+
+export default Services;
